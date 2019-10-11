5d3628c01c822a257d940d7961b3cb6cd40fd1a8
wangkun <wangkun@woodemi.com>
2019-10-11 16:19:07 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ ios/Classes/QuickScanPlugin.m
@@ -0,0 +1,20 @@
+#import "QuickScanPlugin.h"
+
+@implementation QuickScanPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"quick_scan"
+            binaryMessenger:[registrar messenger]];
+  QuickScanPlugin* instance = [[QuickScanPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
