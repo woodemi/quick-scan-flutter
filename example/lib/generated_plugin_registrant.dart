329a4124d666d23cae5b1a1ddfd90600cc78b59d
wangkun <wangkun@woodemi.com>
2020-09-03 06:53:36 +0800

    Update CameraX to beta08
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -5,8 +5,9 @@
 // ignore: unused_import
 import 'dart:ui';
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:quick_scan/quick_scan_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
